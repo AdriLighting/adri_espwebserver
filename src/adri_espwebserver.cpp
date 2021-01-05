54dfb13a9d9d5b82b903d34b9e1f51f2d395f3ae
Grellard Adrien <grelard.adrien@gmail.com>
2021-01-05 20:23:45 +0100

    upd

--- src/adri_espwebserver.cpp
+++ src/adri_espwebserver.cpp
@@ -284,6 +284,7 @@ void adri_webserver::request_param_get(String * result){
 #ifdef ADRI_REPONSE_H
 
 #endif
+const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
 
 void adri_webserver::initialize (int port) {
 
@@ -393,11 +394,44 @@ void adri_webserver::initialize (int port) {
 	}
 	#endif
 
+
 	_server.on("/request_reset", HTTP_GET, [this]() {
 		_server.send(200, "text/plain", "reset");
 		delay(1000);
 		ESP.reset();
 	});
+	_server.on("/update", HTTP_GET, [this]() {
+		_server.sendHeader("Connection", "close");
+		_server.send(200, "text/html", serverIndex);
+	});
+    _server.on("/update", HTTP_POST, [this]() {
+      _server.sendHeader("Connection", "close");
+      _server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+      ESP.restart();
+    }, [this]() {
+      HTTPUpload& upload = _server.upload();
+      if (upload.status == UPLOAD_FILE_START) {
+        Serial.setDebugOutput(true);
+        WiFiUDP::stopAll();
+        Serial.printf("Update: %s\n", upload.filename.c_str());
+        uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+        if (!Update.begin(maxSketchSpace)) { //start with max available size
+          Update.printError(Serial);
+        }
+      } else if (upload.status == UPLOAD_FILE_WRITE) {
+        if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+          Update.printError(Serial);
+        }
+      } else if (upload.status == UPLOAD_FILE_END) {
+        if (Update.end(true)) { //true to set the size to the current progress
+          Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+        } else {
+          Update.printError(Serial);
+        }
+        Serial.setDebugOutput(false);
+      }
+      yield();
+    });
 
 	_server.on("/status", 	HTTP_GET, 		std::bind(&adri_webserver::handleStatus, 		this));
 	_server.on("/list", 	HTTP_GET, 		std::bind(&adri_webserver::handleFileList, 		this));
